f5c9e2a702418f38f3c7da37f7286ae9c26000f2
ChemaLopez <joselo01@ucm.es>
2018-04-09 19:46:14 +0200

    Añadidas primeras clases

---
+++ src/Decision/Decision.java
@@ -0,0 +1,28 @@
+package Decision;
+
+import java.util.ArrayList;
+
+public class Decision {
+
+	
+	private ArrayList<Infor> listaAtributos;
+	private String nombre;
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public ArrayList<Infor> getListaAtributos() {
+		return listaAtributos;
+	}
+	public void setListaAtributos(ArrayList<Infor> listaAtributos) {
+		this.listaAtributos = listaAtributos;
+	}
+	
+	
+}
