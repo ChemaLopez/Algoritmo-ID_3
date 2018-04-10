6a19b6ab5907ddc4adf242b33adf53c451962f03
ChemaLopez <joselo01@ucm.es>
2018-04-10 22:14:46 +0200

    Terminada lectura de archivos

---
+++ src/ID3/ID3.java
@@ -0,0 +1,109 @@
+package ID3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import Decision.Atributo;
+import Decision.Decision;
+
+public class ID3 {
+
+	private ArrayList<Atributo> listaAtributos;
+	
+	
+	
+	public ID3(){
+		listaAtributos = new ArrayList<Atributo>();
+		
+	}
+
+	
+	public void leerListaAtributos(String archivo) throws IOException{
+		
+		String currentDir = new File("").getAbsolutePath();
+	
+	    File newFile = new File(currentDir,archivo);
+		
+		String cadena;
+		FileReader lector = new FileReader(newFile);
+		BufferedReader buffer = new BufferedReader(lector);
+		 
+		//LEER EL ARCHIVO HASTA EL FINAL DE ARCHIVO
+		while((cadena = buffer.readLine())!=null) {
+			//PARSEAR CADA LINEA
+			String op[] = cadena.split(",");
+			for(String decision : op) {
+				
+				Atributo nuevoAtributo = new Atributo(decision);
+				listaAtributos.add(nuevoAtributo);
+			}
+			
+		}
+		      buffer.close();
+		
+	}
+	
+	
+	
+	
+public void leerOpciones(String archivo) throws IOException{
+		
+		String currentDir = new File("").getAbsolutePath();
+	
+	    File newFile = new File(currentDir,archivo);
+		
+		String cadena;
+		FileReader lector = new FileReader(newFile);
+		BufferedReader buffer = new BufferedReader(lector);
+		 
+		//LEER EL ARCHIVO HASTA EL FINAL DE ARCHIVO
+		while((cadena = buffer.readLine())!=null) {
+			//PARSEAR CADA LINEA
+			String op[] = cadena.split(",");
+			int i =0;
+			int pos=0;
+			for(String decision : op) {
+				Boolean encontrado=false;
+				ArrayList<Decision>decisiones = listaAtributos.get(i).getOpciones();
+				for(Decision deci : decisiones) {
+					if(deci.getNombre().compareToIgnoreCase(decision)==0){
+						encontrado=true;
+						pos++;
+					}
+					
+				}
+				if(!encontrado)
+					listaAtributos.get(i).addOpcion(decision);
+			
+				i++;
+			}
+			
+		}
+		      buffer.close();
+		
+	}
+	
+
+public String toString() {
+	
+	String cadena="";
+	
+	
+	for(Atributo atributo :listaAtributos){
+			cadena+=' '+atributo.getName();
+			for(Decision deci : atributo.getOpciones()) {
+				cadena+=' '+deci.getNombre();
+				
+			}
+		cadena+=("\n");
+	}
+	
+	return cadena;
+}
+
+
+	
+}
